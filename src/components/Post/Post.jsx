b2b70753b37609a18af844e23e4b9bef28866132
Handrys <handry.hard228@gmail.com>
2022-01-07 18:02:03 +0200

    start

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,20 @@
+function Post() {
+    return ( <div className="post">
+        <div className="post__img"><img src="https://kod.ru/content/images/size/w1250/2022/01/Tesla.png" alt="" /></div>
+        <div className="post__content">
+            <div className="post__content-title">В туннелях Vegas Loop Илона Маска образовались пробки из электромобилей Tesla</div>
+            <div className="post__content-author">
+                <div className="post-author__avatar"><img src="https://kod.ru/content/images/size/w50/2020/04/------2.jpg" alt="" /></div>
+                <div className="post-author__info">
+                    <div className="post-author-info__name">Игорь Савкин</div>
+                    <div className="post-author-info__when">
+                        <div className="post-author-info__when-date">Jan 07, 2022 </div>
+                        <div className="post-author-info__when-time">14:28</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> );
+}
+
+export default Post;
