3a5be8d296d66edb26404b0f3cead1eeb29c86bc
Handrys <handry.hard228@gmail.com>
2023-01-27 12:16:23 +0200

    Update projectData.js

--- src/shared/projectData.js
+++ src/shared/projectData.js
@@ -1,5 +1,4 @@
-export const backendLink = 'https://kodim-blog.herokuapp.com/';
-/* export const backendLink = 'https://itcite.onrender.com/'; */
+export const backendLink = 'https://itcite.onrender.com/';
 /* export const backendLink = 'http://localhost:4444/'; */
 export const defaultAvatar = 'https://res.cloudinary.com/divogmzjb/image/upload/v1672680378/userImages/user-avatar_rfci7l.webp'
 export const defaultPostImg = 'https://res.cloudinary.com/divogmzjb/image/upload/v1672347758/postImages/tqttgimngxhpzjy8562d.png'
